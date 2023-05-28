8afdce0acf369c11845296e3bc9a16d9dd878ab7
Riya Ranka <81897389+RiyaRanka@users.noreply.github.com>
2023-05-28 20:32:39 +0530

    Update compare_string_without_strcmp.c

--- compare_string_without_strcmp.c
+++ compare_string_without_strcmp.c
@@ -1,33 +1,32 @@
-#include<stdio.h>
-int compareString(char[],char[]);
-int main(){
-int flag;
-char str1[80],str2[80];
-printf("Please enter first string : ");
-gets(str1);
-printf("Please enter second string : ");
-gets(str2);
-flag=compareString(str1,str2);
-if(flag==0){
-    printf("Both strings are equal.\n");
-}
-else{
-    printf("Both strings are not equal.");
-}
-return 0;
-}
-
-int compareString(char str1[],char str2[]){
-    int i=0;
-    while(str1[i]==str2[i]){
-        if(str1[i]=='\0'|| str2[i]=='\0')
-        break;
+#include <iostream>
+using namespace std;
+int compareString(const char str1[], const char str2[]) {
+    int i = 0;
+    while (str1[i] == str2[i]) {
+        if (str1[i] == '\0' || str2[i] == '\0')
+            break;
         i++;
     }
-    if(str1[i]=='\0' && str2[i]=='\0'){
+    if (str1[i] == '\0' && str2[i] == '\0') {
         return 0;
     }
-    else{
+    else {
         return 1;
     }
+}
+
+int main() {
+    int flag;
+    char str1[50], str2[50];
+    cout << "Please enter the first string: ";
+    cin.getline(str1, 50);
+    cout << "Please enter the second string: ";
+    cin.getline(str2, 50);
+    flag = compareString(str1, str2);
+    if (flag == 0) {
+        cout << "Both strings are equal." << endl;
+    } else {
+        cout << "Both strings are not equal." << endl;
+    }
+    return 0;
 }
